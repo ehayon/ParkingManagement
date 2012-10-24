22b286c1799b0debc87a69a59fda92f976e3ee28
Ethan Hayon <ehayon@gmail.com>
2012-10-24 19:54:33 -0400

    finished database schema

--- db/schema.sql
+++ db/schema.sql
@@ -27,5 +27,35 @@ CREATE TABLE IF NOT EXISTS `parkinglots`(
 	PRIMARY KEY(`id_parkinglot`)
 );
 	
+CREATE TABLE IF NOT EXISTS `activities`(
+	`id_activity` INT NOT NULL AUTO_INCREMENT,
+	`id_parkinglot` INT,
+	`timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	`description` TEXT,
+	PRIMARY KEY(`id_activity`),
+	FOREIGN KEY(`id_parkinglot`) REFERENCES parkinglots(`id_parkinglot`)
+);
 
+CREATE TABLE IF NOT EXISTS `comments`(
+	`id_comment` INT NOT NULL AUTO_INCREMENT,
+	`id_user` INT,
+	`comment` TEXT,
+	PRIMARY KEY(`id_comment`),
+	FOREIGN KEY(`id_user`) REFERENCES users(`id_user`)
+);
 
+CREATE TABLE IF NOT EXISTS `users_roles`(
+	`id_user` INT,
+	`id_role` INT,
+	PRIMARY KEY(`id_user`,`id_role`),
+	FOREIGN KEY(`id_user`) REFERENCES users(`id_user`),
+	FOREIGN KEY(`id_role`) REFERENCES roles(`id_role`)
+);
+
+CREATE TABLE IF NOT EXISTS `user_parkinglots_subscriptions`(
+	`id_user` INT,
+	`id_parkinglot` INT,
+	PRIMARY KEY(`id_user`,`id_parkinglot`),
+	FOREIGN KEY(`id_user`) REFERENCES users(`id_user`),
+	FOREIGN KEY(`id_parkinglot`) REFERENCES parkinglots(`id_parkinglot`)
+);
